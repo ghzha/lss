bae6e1046e311e6fdcd070489e3d6213c9b5742f
ghzhan <roy.zhan@qq.com>
2024-08-18 00:32:02 +0800

    change the display style

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 // use owo_colors::OwoColorize;
 use colored::Colorize;
-use std::{cmp::Reverse, env, time::Instant};
+use std::{cmp::Ordering, env, time::Instant};
 use tabled::{
     settings::{object::Columns, Format, Style},
     Table, Tabled,
@@ -9,19 +9,32 @@ use walkdir::{DirEntry, WalkDir};
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Tabled)]
 struct FileItem {
+    #[tabled(display_with("Self::display_name", self))]
     #[tabled(rename = "Name")]
     name: String,
     #[tabled(skip)]
     size: u64,
     #[tabled(rename = "Size")]
     formatted_size: String,
+    #[tabled(skip)]
+    is_dir: bool,
 }
 impl FileItem {
-    fn new(name: &str, size: u64) -> Self {
+    fn new(name: &str, size: u64, is_dir: bool) -> FileItem {
         FileItem {
             name: name.to_string(),
             size: size,
             formatted_size: formatted_size(size),
+            is_dir: is_dir,
+        }
+    }
+    fn display_name(self: &Self) -> String {
+        if self.is_dir {
+            format!("\u{f4d3} {}", self.name).blue().to_string()
+            // self.name.red().to_string()
+        } else {
+            format!("\u{f15b} {}", self.name).yellow().to_string()
+            // self.name.yellow().to_string()
         }
     }
 }
@@ -49,7 +62,8 @@ impl From<DirEntry> for FileItem {
             .and_then(|name| name.to_str())
             .unwrap_or("");
         let size = get_entry_size(&entry);
-        FileItem::new(name, size)
+        let is_dir = entry.file_type().is_dir();
+        FileItem::new(name, size, is_dir)
     }
 }
 
@@ -69,7 +83,6 @@ fn get_entry_size(entry: &DirEntry) -> u64 {
 
 fn list_dir(path: &str) {
     let start = Instant::now();
-
     let mut items: Vec<_> = WalkDir::new(path)
         .max_depth(1)
         .min_depth(1)
@@ -77,26 +90,28 @@ fn list_dir(path: &str) {
         .filter_map(Result::ok)
         .map(FileItem::from)
         .collect();
-    items.sort_by(|a, b| Reverse(a.size).cmp(&Reverse(b.size)));
+    items.sort_by(|a, b| {
+        match (b.is_dir, a.is_dir) {
+            (true, false) => Ordering::Greater, // 文件夹排在前面
+            (false, true) => Ordering::Less,    // 文件排在后面
+            _ => b.size.cmp(&a.size),           // 同类型的按大小排序，降序
+        }
+    });
+
+    // items.sort_by(|a, b| Reverse(a.size).cmp(&Reverse(b.size)));
     let table = Table::new(&items)
-        .with(Style::rounded())
-        .modify(Columns::single(0), Format::content(|s| s.red().to_string()))
-        .modify(
-            Columns::single(1),
-            Format::content(|s| s.blue().to_string()),
-        )
+        .with(Style::sharp())
+        .modify(Columns::single(1), Format::content(|s| s.to_string()))
         .to_string();
 
     println!("{}", table);
-
-    println!("\t共耗时: {:.2?}", start.elapsed())
+    println!("\n共耗时: {:.2?}", start.elapsed())
 }
 
 fn main() {
     let mut args = env::args();
     args.next();
     let path = args.next().unwrap_or(r"./".to_string());
-
     list_dir(path.as_str());
 }
 
@@ -105,6 +120,6 @@ mod tests {
     use super::*;
     #[test]
     fn test_list_dir() {
-        list_dir(r"D:\codes")
+        list_dir(r"./")
     }
 }
